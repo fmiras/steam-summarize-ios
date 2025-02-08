fffdfb0a440bff060cabb0f5a2f74513c80dc6cc
Federico Miras <miras.federico@gmail.com>
2025-02-08 16:08:00 -0300

    Initial Commit

---
+++ SteamSummarize/SteamSummarizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteamSummarizeApp.swift
+//  SteamSummarize
+//
+//  Created by Federico Miras on 08/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SteamSummarizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SteamSummarizeTests/SteamSummarizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SteamSummarizeTests.swift
+//  SteamSummarizeTests
+//
+//  Created by Federico Miras on 08/02/2025.
+//
+
+import Testing
+@testable import SteamSummarize
+
+struct SteamSummarizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
